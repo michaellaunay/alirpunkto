ce70b97a96f921aa89b1c6f1c74f75196dff3a6d
Michaël LAUNAY <michaellaunay@ecreall.com>
2023-10-25 20:28:43 +0200

    Debug the logout function

--- alirpunkto/views/logout.py
+++ alirpunkto/views/logout.py
@@ -2,16 +2,10 @@
 # author: Michaël Launay
 # date: 2023-07-28
 
-import datetime
 import logging
-from typing import Union
 from pyramid.view import view_config
 from pyramid.httpexceptions import HTTPFound
 from pyramid.security import forget
-from ldap3 import Server, Connection, ALL, NTLM
-from .. import _
-from .. import LDAP_SERVER, LDAP_OU, LDAP_BASE_DN, LDAP_LOGIN, LDAP_PASSWORD
-from ..models.users import User
 
 @view_config(route_name='logout')
 def logout_view(request):
@@ -20,17 +14,18 @@ def logout_view(request):
     Args:
         request (pyramid.request.Request): the request
     """
-    logged_in = request.params.get('logged_in', False)
-    site_name = request.params.get('site_name', 'AlirPunkto')
     username = request.params.get('username', "")
+    if username:
+        del request.session['username']
     user = request.session.get('user', None)
-    if 'form.submitted' in request.params:
-        if user is not None:
-            del request.session['user']
-            request.session['logged_in'] = False
-            request.session['created_at'] = None
-            request.session['site_name'] = site_name
-        else:
-            request.session['logged_in'] = False
+    if user is not None:
+        # log the user is logging out
+        log = logging.getLogger('alirpunkto')
+        log.info(f"User {user} is logging out")
+        del request.session['user']
+        request.session['logged_in'] = False
+        request.session['created_at'] = None
+    else:
+        request.session['logged_in'] = False
     forget(request)
     return HTTPFound(location=request.route_url('home'))
