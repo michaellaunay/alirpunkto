cb0321efad81d3be98f476727a89751772d6499d
Michael Launay <michaellaunay@logikascium.com>
2024-06-21 21:45:03 +0000

    Fix sso token deserialization

--- alirpunkto/views/sso_login.py
+++ alirpunkto/views/sso_login.py
@@ -4,6 +4,7 @@ from keycloak import KeycloakOpenID
 from pyramid.httpexceptions import HTTPFound
 from alirpunkto.constants_and_globals import (
     _,
+    log,
     KEYCLOAK_CLIENT_ID,
     KEYCLOAK_REALM,
     KEYCLOAK_SERVER_URL,
@@ -11,30 +12,60 @@ from alirpunkto.constants_and_globals import (
     KEYCLOAK_REDIRECT_PATH,
 )
 from alirpunkto.secret_manager import get_secret
+import jwt
 
 @view_config(route_name='sso_login')
 def sso_login_view(request):
-    keycloak_openid = KeycloakOpenID(server_url=KEYCLOAK_SERVER_URL,
-                                     client_id=get_secret(KEYCLOAK_CLIENT_ID),
-                                     realm_name=KEYCLOAK_REALM,
-                                     client_secret_key=get_secret(KEYCLOAK_CLIENT_SECRET))
-    auth_url = keycloak_openid.auth_url(redirect_uri=request.route_url(KEYCLOAK_REDIRECT_PATH))
+    keycloak_openid = KeycloakOpenID(
+        server_url=KEYCLOAK_SERVER_URL,
+        client_id=get_secret(KEYCLOAK_CLIENT_ID),
+        realm_name=KEYCLOAK_REALM,
+        client_secret_key=get_secret(KEYCLOAK_CLIENT_SECRET)
+    )
+    auth_url = keycloak_openid.auth_url(
+        redirect_uri=request.route_url(KEYCLOAK_REDIRECT_PATH),
+        scope='openid profile email'
+    )
     return HTTPFound(location=auth_url)
 
 @view_config(route_name=KEYCLOAK_REDIRECT_PATH)
 def callback_view(request):
-    keycloak_openid = KeycloakOpenID(server_url=KEYCLOAK_SERVER_URL,
-                                     client_id=get_secret(KEYCLOAK_CLIENT_ID),
-                                     realm_name=KEYCLOAK_REALM,
-                                     client_secret_key=get_secret(KEYCLOAK_CLIENT_SECRET))
+    keycloak_openid = KeycloakOpenID(
+        server_url=KEYCLOAK_SERVER_URL,
+        client_id=get_secret(KEYCLOAK_CLIENT_ID),
+        realm_name=KEYCLOAK_REALM,
+        client_secret_key=get_secret(KEYCLOAK_CLIENT_SECRET)
+    )
 
     code = request.params.get('code')
-    token = keycloak_openid.token(grant_type='authorization_code', code=code, redirect_uri=request.registry.settings['keycloak.redirect_uri'])
-    userinfo = keycloak_openid.userinfo(token['access_token'])
-    
-    request.session['sso_token'] = token
-    request.session['sso_user'] = userinfo
-
+    redirect_uri = request.route_url(KEYCLOAK_REDIRECT_PATH)
+    token = keycloak_openid.token(
+        grant_type='authorization_code',
+        code=code,
+        redirect_uri=redirect_uri
+    )
+    access_token = token["access_token"]
+    at_head = jwt.get_unverified_header(access_token)
+    algo = at_head['alg']
+    # Get the sso server public key
+    public_key = f"""-----BEGIN PUBLIC KEY-----
+{keycloak_openid.public_key()}
+-----END PUBLIC KEY-----"""
+    try:
+    # Decode and verify the JWT
+        decoded_payload = jwt.decode(
+            access_token,
+            public_key,
+            algorithms=[algo],
+            audience=[get_secret(KEYCLOAK_CLIENT_ID), 'account']
+        )
+        log.debug("Verified sso token payload: ", decoded_payload)
+    except jwt.ExpiredSignatureErrori as err:
+        log.debug("The sso token has expired")
+    except jwt.InvalidAudienceError as err:
+        log.warning(f"Inavalide audience in sso token: {err}")
+    except jwt.InvalidTokenError as err:
+        log.warning(f"Invalid sso token: {err}")
     #@TODO from uid retrieve the ldap user and log him in
     
     return HTTPFound(location='/')
