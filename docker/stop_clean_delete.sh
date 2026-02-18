f45408ed9ea5890b7c013a8e9a452a81087ad529
Michaël LAUNAY <michaellaunay@ecreall.com>
2026-02-18 11:41:04 +0100

    WIP

---
+++ docker/stop_clean_delete.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+
+echo "docker stop alirpunkto-ldap"
+docker stop alirpunkto-ldap || true
+echo "docker rm alirpunkto-ldap"
+docker rm alirpunkto-ldap  || true
+echo "docker image rm alirpunkto-ldap:latest"
+docker image rm alirpunkto-ldap:latest || true
+echo "docker volume rm alirpunkto_ldap_etc"
+docker volume rm alirpunkto_ldap_etc || true
+echo "docker volume rm alirpunkto_ldap_var"
+docker volume rm alirpunkto_ldap_var || true
+
+echo "Cleanup finished."
+
